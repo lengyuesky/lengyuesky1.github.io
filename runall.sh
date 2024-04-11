d57c370ba1dae0361a97ab6e3ac17a1e424da920
lengyuesky <101855543+lengyuesky@users.noreply.github.com>
2024-04-11 21:46:49 +0800

    Update runall.sh

--- runall.sh
+++ runall.sh
@@ -3,5 +3,5 @@ if ps aux | grep 'apoolminer' | grep -q 'apool.io'; then
     echo "ApoolMiner already running."
     exit 1
 else
-    nohup ./apoolminer --account CP_ft0xn8eci4 --pool aws.lengyuesky.me:3334 >> qubic.log 2>&1 &
+    nohup ./apoolminer --account CP_ft0xn8eci4 --pool qubic1.hk.apool.io:3334 >> qubic.log 2>&1 &
 fi

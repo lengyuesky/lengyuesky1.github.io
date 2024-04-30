7b8ae2d548cbac20e8c7c4d6fa37c63cafbbd080
lengyuesky <101855543+lengyuesky@users.noreply.github.com>
2024-04-30 23:59:13 +0800

    Update runnode.sh

--- runnode.sh
+++ runnode.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
 
-# 循环无限次运行程序
 while true; do
-    # 运行程序，并通过tee命令同时输出到屏幕和传递给一个子shell
-    ./node | tee /dev/tty | grep -q "ERROR" &
-    pid=$!  # 获取grep进程的PID
+    # 使用timeout命令运行程序，设置时间为5分钟
+    timeout 13m ./node
 
-    # 等待grep进程结束
-    wait $pid
-    exit_status=$?
+    # 程序运行时间结束后，这里可以加入任何你需要执行的清理工作
+    echo "程序已运行5分钟，正在重启..."
 
-    # 检查grep的退出状态
-    if [ $exit_status -eq 0 ]; then
-        echo "检测到 'ERROR'，即将停止并重启程序..."
-        # 杀死node程序
-        kill $pid_node
-        wait $pid_node  # 等待node程序结束
-        sleep 1s  # 发现错误后，暂停1秒钟再重启
-    else
-        echo "程序正常结束，未检测到 'ERROR'。"
-        break  # 如果程序结束且未检测到 'ERROR'，退出循环
-    fi
+    # 如果需要，在重启之前暂停
+    sleep 1s
+
+    # 重启程序
+    echo "正在重启程序..."
 done
+

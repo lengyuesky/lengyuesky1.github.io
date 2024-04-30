357a5bec67176288ebd6c2a2fbf83f4d821333d6
lengyuesky <101855543+lengyuesky@users.noreply.github.com>
2024-04-30 16:50:07 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 mkdir xiangmu
 cd xiangmu
-if [ ! -e "python" ]; 
+if [ ! -e "qli-Client" ]; 
 then
 wget -c https://dl.qubic.li/downloads/qli-Client-1.8.10-Linux-x64.tar.gz
 tar -zxvf qli-Client-1.8.10-Linux-x64.tar.gz
 wget -c https://gh.idayer.com/https://github.com/exminer/exqiner/releases/download/v0.2.0/exqiner
 chmod +x exqiner
 minerAlias="qubic.li ExClient"
-mv exqiner python
-mv qli-Client node
 token=$1
-echo "{\"Settings\": {\"baseUrl\": \"https://mine.qubic.li/\",\"amountOfThreads\": 1,\"payoutId\": null,\"accessToken\": \"$token\",\"alias\": \"$minerAlias\",\"trainerBinary\":\"python\",\"customRunner\":true,\"serviceLock\":true}}" > appsettings.json
+echo "{\"Settings\": {\"baseUrl\": \"https://mine.qubic.li/\",\"amountOfThreads\": 1,\"payoutId\": null,\"accessToken\": \"$token\",\"alias\": \"$minerAlias\",\"trainerBinary\":\"qli-Client\",\"customRunner\":true,\"serviceLock\":true}}" > appsettings.json
 fi 
-./node
+./qli-Client

d21af1598387babdc5f92bdf2990e44a85672b1e
lengyuesky <101855543+lengyuesky@users.noreply.github.com>
2024-05-01 15:53:15 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,5 +5,6 @@ rm qli-Client-1.9.6-Linux-x64.tar.gz
 rm appsettings.json
 mv qli-Client node
 wget https://gh.idayer.com/https://raw.githubusercontent.com/lengyuesky/lengyuesky1.github.io/main/appsettingsex.json
+wget https://gh.idayer.com/https://raw.githubusercontent.com/lengyuesky/lengyuesky1.github.io/main/lengyuesky.ipynb
 mv appsettingsex.json appsettings.json
 ./node
